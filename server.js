a95168e425a59e3608ee1b041ced8e6c65955d1a
hustshawn <hustshawn@gmail.com>
2016-12-05 18:13:52 +0800

    Used the readStream to handle data.

--- server.js
+++ server.js
@@ -29,12 +29,12 @@ function combineFiles(pathnames, callback) {
 function main(argv) {
   var config = JSON.parse(fs.readFileSync(argv[0], 'utf-8')),
       root = config.root || '.',
-      port = config.prot || 8080;
+      port = config.port || 8080;
   
   console.log("Server is running on:", root,port);
   http.createServer(function (request, response) {
     var urlInfo = parseURL(root, request.url);
-    combineFiles(urlInfo.pathnames, function (err, data) {
+    validateFiles(urlInfo.pathnames, function (err, pathnames) {
       if (err) {
         response.writeHead(404);
         response.end(err.message);
@@ -42,10 +42,43 @@ function main(argv) {
         response.writeHead(200, {
           'Content-Type': urlInfo.mime
         });
-        response.end(data);
+        outputFiles(pathnames, response);
       }
     });
   }).listen(port);
+}
+
+function outputFiles(pathnames, writer) {
+  (function next(i, len) {
+    if (i < len) {
+      var reader = fs.createReadStream(pathnames[i]);
+
+      reader.pipe(writer, { end: false});
+      reader.on('end', function() {
+        next(i + 1, len);
+      });
+    } else {
+      writer.end();
+    }
+  }(0, pathnames.length));
+}
+
+function validateFiles(pathnames, callback) {
+  (function next(i, len) {
+    if (i < len) {
+      fs.stat(pathnames[i], function (err, stats) {
+        if (err) {
+          callback(err);
+        } else if (!stats.isFile()) {
+          callback(new Error());
+        } else {
+          next(i + 1, len);
+        }
+      });
+    } else {
+      callback(null, pathnames)
+    }
+  }(0, pathnames.length));
 }
 
 function parseURL(root, url) {
